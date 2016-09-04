f5d2a35d33854b89caeac3d8ce17d4071bb74258
Diego Rodriguez <dxanderg@gmail.com>
2016-09-04 13:18:57 -0500

    Platzigram Axios

--- Platzigram/server.js
+++ Platzigram/server.js
@@ -18,6 +18,35 @@ app.get('/signin', function (req, res) {
 	res.render('index');
 })
 
+app.get('/api/pictures', function (req, res){
+  var pictures = [
+    {
+      user: {
+        username: 'dxanderg',
+        avatar: 'https://lh3.googleusercontent.com/2ZxNNt51Eaizw_tmrDT-Tp9hJ1lLBwbcRGhB4_ynb_hcAOm1xBVTFCC7pq9kKUT6fma503V9AA=w1440-h900-rw-no'
+      },
+      url: 'office.jpg',
+      likes: 0,
+      liked: false,
+      createdAt: new Date().getTime()
+    },
+    {
+      user: {
+        username: 'dxanderg',
+        avatar: 'https://lh3.googleusercontent.com/2ZxNNt51Eaizw_tmrDT-Tp9hJ1lLBwbcRGhB4_ynb_hcAOm1xBVTFCC7pq9kKUT6fma503V9AA=w1440-h900-rw-no'
+      },
+      url: 'office.jpg',
+      likes: 2,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate() - 10)
+    }
+  ];
+
+  setTimeout(function () {
+  	res.send(pictures);
+  }, 2000)
+})
+
 app.listen(3000, function(err) {
 	if (err) return console.log('Ocurrio un Error'), process.exit(1);
 
--- Platzigram/src/homepage/index.js
+++ Platzigram/src/homepage/index.js
@@ -2,33 +2,24 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header = require('../header');
 
-page('/', function (ctx, next) {
+page('/', header, loadPictures, function (ctx, next) {
   title('Platzigram');
   var main = document.getElementById('main-container');
 
-  var pictures = [
-    {
-      user: {
-        username: 'dxanderg',
-        avatar: 'https://lh3.googleusercontent.com/2ZxNNt51Eaizw_tmrDT-Tp9hJ1lLBwbcRGhB4_ynb_hcAOm1xBVTFCC7pq9kKUT6fma503V9AA=w1440-h900-rw-no'
-      },
-      url: 'office.jpg',
-      likes: 0,
-      liked: false,
-      createdAt: new Date()
-    },
-    {
-      user: {
-        username: 'dxanderg',
-        avatar: 'https://lh3.googleusercontent.com/2ZxNNt51Eaizw_tmrDT-Tp9hJ1lLBwbcRGhB4_ynb_hcAOm1xBVTFCC7pq9kKUT6fma503V9AA=w1440-h900-rw-no'
-      },
-      url: 'office.jpg',
-      likes: 2,
-      liked: true,
-      createdAt: new Date().setDate(new Date().getDate() - 10)
-    }
-  ];
-
-  empty(main).appendChild(template(pictures));
+  empty(main).appendChild(template(ctx.pictures));
 })
+
+
+function loadPictures(ctx, next){
+  request
+    .get('/api/pictures')
+    .end(function (err, res) {
+      if (err) return console.log(err);
+
+       ctx.pictures = res.body;
+       next();
+    })
+}
