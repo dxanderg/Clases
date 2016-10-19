7bc192b3e4ffe7036beaa8e3cb2b48db69c9a1cf
Diego Rodriguez <dxanderg@gmail.com>
2016-10-19 05:26:01 -0500

    Tvify - Server and npm public

--- Tvify/index.js
+++ Tvify/index.js
@@ -1,77 +1,82 @@
+import $ from 'jquery';
+
 $(function() {
-	var $tvShowContainer = $('#app-body').find('.tv-shows');
+  var $tvShowsContainer = $('#app-body').find('.tv-shows');
 
-	function renderShows(shows){
-		shows.forEach(function (show) {
-				var article = template
-					.replace(':name:', show.name)
-					.replace(':img:', show.image.medium)
-					.replace(':summary:', show.summary)
-					.replace(':img alt:', show.name + " logo")
+  $tvShowsContainer.on('click', 'button.like', function (ev) {
+  	var $this = $(this);
+  	$this.animate({
+  		'fontSize': '30px'
+  	}, 'fast');
+  })
 
-					var $article = $(article)
-					$article.hide();
-					$tvShowContainer.append($article.show())
-		})
-	}
+  function renderShows(shows) {
+    $tvShowsContainer.find('.loader').remove();
+    shows.forEach(function (show) {
+      var article = template
+        .replace(':name:', show.name)
+        .replace(':img:', show.image ? show.image.medium : '')
+        .replace(':summary:', show.summary)
+        .replace(':img alt:', show.name + " Logo")
 
-	$('#app-body')
-		.find('form')
-		.submit(function (ev) {
-			ev.preventDefault();
-			var busqueda = $(this)
-				.find('input[type="submit"]')
-				.val();
-			$.ajax({
-				url: 'http://api.tvmaze.com/search/shows',
-				data: { q:busqueda },
-				success: function (shows, textStatus, xhr) {
-					$tvShowContainer.find('.tv-show').remove();
-					var shows = res.map(function(el) {
-						return el.show;
-					})
-					renderShows(shows);
-				}
-			})
-		})
+      var $article = $(article)
+      $article.hide();
+      $tvShowsContainer.append($article.fadeIn(1000));
+    })
+  }
 
-	var template = '<article class="tv-show">' +
-					'<div class="left img-container">' +
-						'<img src=":img:" alt=":img alt:">' + 
-					'</div>' +
-					'<div class="right info">' +
-						'<h1>:name:</h1>' +
-						'<p>:summary:</p>' +
-					'</div>' +
-				'</article>';
+  /**
+   * Submit serach form
+   */
 
-	$.ajax({
-		url: 'http://api.tvmaze.com/shows',
-		success: function (shows, textStatus, xhr) {
-			
-			$tvShowContainer.find('.loader').remove();
-			renderShows(shows);
-		}
+  $('#app-body')
+    .find('form')
+    .submit(function (ev) {
+      ev.preventDefault();
+      var busqueda = $(this)
+        .find('input[type="text"]')
+        .val();
 
-	})
+      $tvShowsContainer.find('.tv-show').remove()
+      var $loader = $('<div class="loader">');
+      $loader.appendTo($tvShowsContainer);
+      $.ajax({
+        url: 'http://api.tvmaze.com/search/shows',
+        data: { q: busqueda },
+        success: function (res, textStatus, xhr) {
+          $loader.remove();
+          var shows = res.map(function (el) {
+            return el.show;
+          })
+
+          renderShows(shows);          
+        }
+      })
+    })
+
+  var template = '<article class="tv-show">' +
+          '<div class="left img-container">' +
+            '<img src=":img:" alt=":img alt:">' +
+          '</div>' +
+          '<div class="right info">' +
+            '<h1>:name:</h1>' +
+            '<p>:summary:</p>' +
+            '<button class="like">💖</button>' +
+          '</div>' +
+        '</article>';
+
+  if (!localStorage.shows) {
+    $.ajax('http://api.tvmaze.com/shows')
+      .then(function (shows) {
+        $tvShowsContainer.find('.loader').remove();
+        localStorage.shows = JSON.stringify(shows);
+        renderShows(shows);
+      })
+  } else {
+    renderShows(JSON.parse(localStorage.shows));
+  }
+  
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
