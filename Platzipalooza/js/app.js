935afeed14189ab06196c22817e50e5a5b82d333
Diego Rodriguez <dxanderg@gmail.com>
2016-12-23 11:40:54 -0500

    Platzipalooza Final

--- Platzipalooza/js/app.js
+++ Platzipalooza/js/app.js
@@ -1,1 +1,33 @@
 $(document).foundation()
+
+$('[data-toggle-dia]').click(function (ev) {
+	const panel = $(this).data('toggleDia')
+	$('#lineup-tabs').foundation('selectTab', panel)
+})
+
+var $offCanvas = $('#offCanvas')
+var $sticky = $('#sticky')
+
+$offCanvas.find('li').click( function (ev){
+	$offCanvas.foundation('close')
+	setTimeout(function(){
+		$sticky.css('left', '0px')
+	}, 300)
+})
+
+
+// $(document).foundation()
+
+// $('[data-toggle-dia]').click(function () {
+//   $('#lineup-tabs').foundation('selectTab', $(this).data('toggleDia'))
+// });
+
+// var $offCanvas = $('#offCanvas');
+// var $sticky = $('#sticky');
+
+// $offCanvas.find('li').click(() => {
+//   $offCanvas.foundation('close');
+//   setTimeout(() => {
+//     $sticky.css('left', '0px');
+//   }, 300)
+// })
